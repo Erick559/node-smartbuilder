827bcb45ce81d87cbe8c5a3ccc13ff2fad807200
Erick559 <erickungu82@gmail.com>
2024-08-15 13:41:14 +0300

    assign values to the model parameters

---
+++ test.js
@@ -0,0 +1,11 @@
+const data = {num1:1,num2:2}
+fetch('https://node-smartbuilder.onrender.com/calculate',{
+  method:'POST',
+  headers: {
+    'Content-Type':'application/json',
+  },
+  body:JSON.stringify(data),
+})
+.then(response=> response.json())
+.then(result => alert(JSON.stringify(result.message)))
+.catch(error => alert(error))
