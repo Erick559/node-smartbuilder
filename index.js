681bd74a8125bc8ca6efa71b173c0520763a9cb2
Erick559 <erickungu82@gmail.com>
2024-09-16 18:52:51 +0300

    install pdfkit

--- index.js
+++ index.js
@@ -23,29 +23,6 @@ mongoose.connect(mongoURL, {
 // A get http request that gets root url and outputs a html output, in this case a paragraph with the text below.
 app.get('/',(req,res)=>res.type('html').send(`<p>Server connection for the custom function</p>`));
 
-// A get http request that will be used to test the server connection to the smartbuilder custom functions. 
-app.get('/ping',(req,res)=> {
-    res.status(200).json({message: 'Server connected successfully',status: 'OK'});
-});
-
-// A post http request that returns the sum of two numbers from the smartbuilder input fields.
-app.post('/calculate',async(req,res)=> {
-    const {num1, num2} = req.body;
-    // if(typeof(num1) !== 'number' || typeof(num2) !== 'number'){
-    //     res.status(400).json({message:"Invalid inputs. Number 1 and Number 2 must be numbers"})
-    // }
-
-    const result = num1 + num2;
-
-    try {
-        const test = new Test({num1:num1,num2:num2,result:result})
-        await test.save();
-
-        res.status(200).json({message:`Test Calculation successfully saved: ${num1}+${num2} = ${result}`,result,status:'OK'});
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-})
 
 app.post('/saveStudentDetails', async (req, res) => {
     const { first_name, last_name, score } = req.body;  // Match keys with the client's payload
